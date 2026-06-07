7477f8326d93b79edbc4513f1db8abc7a5fe4030
Oliver Ah You <ahyouoliver9@gmail.com>
2026-06-07 19:52:48 +0400

    W2D3DC done 7/6/2026

--- W2/W2D3/W2D3DC.py
+++ W2/W2D3/W2D3DC.py
@@ -1,11 +1,11 @@
 import random
 list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
-print(list_of_numbers)
 target_number = 3728
-numbers = set()
-target_sum = number_one + number_two
-for number in target_sum:
-    number_one = num
-    number_two = target_number - num
-    if number_two in numbers:
-        print(f'Match found: {number_one} + {number_two} = {target_number}')
+seen = set()
+for number_one in list_of_numbers:
+    number_two = target_number - number_one
+
+    if number_two in seen:
+        print(f"{number_one} and {number_two} sums up to {target_number}")  
+    seen.add(number_one)
+print(seen)
