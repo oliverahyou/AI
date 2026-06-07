1216e3f1355d91c3d58ed86fad6ee4bf158fab60
Oliver Ah You <ahyouoliver9@gmail.com>
2026-06-07 18:24:11 +0400

    7/6/2026

---
+++ W2/W2D4/W2D2MP2.py
@@ -0,0 +1,6 @@
+import random
+
+wordslist = ['correction', 'childish', 'beach', 'python', 'assertive', 'interference', 'complete', 'share', 'credit card', 'rush', 'south']
+word = random.choice(wordslist) 
+
+    ### YOUR CODE STARTS FROM HERE ###
--- W2/W2D6/.ipynb_checkpoints/W2D6EXP-checkpoint.py
+++
@@ -1,7 +0,0 @@
-# Exercise 1: Cats
-class Cat:
-    def __init__(self, cat_name, cat_age):
-        self.name = cat_name
-        self.age = cat_age
-
-cat1 = Cat("Whiskers", 3)
