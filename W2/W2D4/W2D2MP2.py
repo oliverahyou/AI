36da023fd84ce4c325c9c19825ae08d50c59b7e7
Oliver Ah You <ahyouoliver9@gmail.com>
2026-06-07 19:07:14 +0400

    07/06/2026

--- W2/W2D4/W2D2MP2.py
+++ W2/W2D4/W2D2MP2.py
@@ -3,4 +3,5 @@ import random
 wordslist = ['correction', 'childish', 'beach', 'python', 'assertive', 'interference', 'complete', 'share', 'credit card', 'rush', 'south']
 word = random.choice(wordslist) 
 
-    ### YOUR CODE STARTS FROM HERE ###
+
+### YOUR CODE STARTS FROM HERE ###
