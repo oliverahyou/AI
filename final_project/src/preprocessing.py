e64072c46f1c41f0a55970fa867dd4069a01ec79
Oliver Ah You <ahyouoliver9@gmail.com>
2026-08-28 13:09:14 +0400

    update

--- final_project/src/preprocessing.py
+++ final_project/src/preprocessing.py
@@ -10,6 +10,7 @@ from nltk.stem import WordNetLemmatizer
 nltk.download("stopwords")
 nltk.download("punkt")
 nltk.download("wordnet")
+nltk.download("punkt_tab")
 
 
 stop_words = set(stopwords.words("english"))
